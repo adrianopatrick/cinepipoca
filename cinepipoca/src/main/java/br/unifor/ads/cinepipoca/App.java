6687ee2824d08173319f148707dc54f7365bdabc
adrianopatrick <adrianopatrick@gmail.com>
2014-09-19 20:11:50 -0300

    [patrick-cunha] - nova funcionalidade adicionada: funcionalidade de cadastro de usuario

--- cinepipoca/src/main/java/br/unifor/ads/cinepipoca/App.java
+++ cinepipoca/src/main/java/br/unifor/ads/cinepipoca/App.java
@@ -10,4 +10,8 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+    
+    void teste(){
+    	//não faco nada
+    }
 }
---
+++ cinepipoca/src/main/java/br/unifor/ads/cinepipoca/Hello.java
@@ -0,0 +1,9 @@
+package br.unifor.ads.cinepipoca;
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+
+}
